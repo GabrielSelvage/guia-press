79ea28f1cdc9f94ac63282746b3b5295223ca934
selvagedev <gabriel.selvage@hotmail.com>
2021-01-11 06:02:09 +0000

    Primeira Versão

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require ("sequelize");
 
-const connection = new Sequelize ('guiapress', 'selvagedev', 'Elcamorrero27', {
+const connection = new Sequelize ('guiapress', 'selvagedev', 'Amalia123', {
     host: 'mysql742.umbler.com',
     dialect: 'mysql'
 });
